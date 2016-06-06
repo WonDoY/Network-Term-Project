2d189f499595a777389fe3b3f68c629720f0b558
WonDoY <wondy1128@naver.com>
2016-06-06 20:51:57 +0900

    step1 urls
    urls

--- djangoterm/mysite/mysite/urls.py
+++ djangoterm/mysite/mysite/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # omok
+    url(r'^', include('omok.urls')),
+    #localhost:8000으로 요청이 들어오면 omok.urls로 전달
     url(r'^admin/', include(admin.site.urls)),
 ]
--- djangoterm/mysite/omok/urls.py
+++ djangoterm/mysite/omok/urls.py
@@ -1,6 +1,6 @@
 #urls.py
 from django.conf.urls import include, url
-#load ./views
+#import ./views
 from . import views
 
 
@@ -8,6 +8,5 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', views.index)
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
 ]
