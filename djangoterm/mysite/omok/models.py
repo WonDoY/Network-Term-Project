aec94d8e07a36a5186cb7aacb24017a7c3ff901e
sherry92 <guysuh123@naver.com>
2016-06-07 23:23:09 +0900

    room, admin+

--- djangoterm/mysite/omok/models.py
+++ djangoterm/mysite/omok/models.py
@@ -9,3 +9,11 @@ class PlayerInfo(models.Model):
     def __str__(self):
         return self.Playername
     
+
+class Room(models.Model):
+    Playername = models.CharField(max_length = 15)
+    Room_full = models.IntegerField(default=0)
+    Player1 = models.CharField(max_length=15,default = 'Unknown')
+    Player2 = models.CharField(max_length=15,default = 'Unknown')
+
+    
