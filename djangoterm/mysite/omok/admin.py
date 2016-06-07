cd438d77e018bcfa1e5b8ecd3dbbfa556f8f96a6
sherry92 <guysuh123@naver.com>
2016-06-07 23:29:02 +0900

    game board

--- djangoterm/mysite/omok/admin.py
+++ djangoterm/mysite/omok/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import PlayerInfo
 from .models import Room
+from .models import Board
 # Register your models here.
 
 # Omok Object
 
 admin.site.register(PlayerInfo)
 admin.site.register(Room)
+admin.site.register(Board)
--- djangoterm/mysite/omok/models.py
+++ djangoterm/mysite/omok/models.py
@@ -9,11 +9,17 @@ class PlayerInfo(models.Model):
     def __str__(self):
         return self.Playername
     
-
+# 방 정보 class명 Room
 class Room(models.Model):
     Playername = models.CharField(max_length = 15)
     Room_full = models.IntegerField(default=0)
     Player1 = models.CharField(max_length=15,default = 'Unknown')
     Player2 = models.CharField(max_length=15,default = 'Unknown')
+# Player1,2 , 방 가득찬 경우 Room_full
+
+# 게임판 정보 board, num는 방 번호    
+class Board(models.Model):
+    board = models.TextField()
+    num = models.IntegerField(default=0)
 
     
